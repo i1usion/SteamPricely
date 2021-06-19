baa153bb5989c9079f8fecbf256604c24a66f3c1
imakarevich <ldaynekol@gmail.com>
2021-06-19 14:48:35 +0300

    Initial commit

---
+++ SteamPricely/SteamPricely/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SteamPricely
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NavigateCommand = new Command<Type>(async (Type pageType) =>
+            {
+                Page page = (Page)Activator.CreateInstance(pageType);
+                await Navigation.PushAsync(page);
+            });
+
+            BindingContext = this;
+        }
+
+        public ICommand NavigateCommand { private set; get; }
+
+    }
+}
